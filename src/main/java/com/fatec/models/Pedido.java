849c4b53c81d301b1ee6b77f810dc39ae7978d76
Thiago Rizzo <thiagorizzo@gmail.com>
2019-10-26 09:42:36 -0300

    adicionado modelos

--- src/main/java/com/fatec/models/Pedido.java
+++ src/main/java/com/fatec/models/Pedido.java
@@ -1,5 +1,24 @@
 package com.fatec.models;
 
 public class Pedido {
-
+	private int codigo;
+	private double total;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	
+	public double getTotal() {
+		return total;
+	}
+	
+	public void setTotal(double total) {
+		this.total = total;
+	}
+	
+	
 }
--- src/main/java/com/fatec/models/Produto.java
+++ src/main/java/com/fatec/models/Produto.java
@@ -1,5 +1,23 @@
 package com.fatec.models;
 
-public class Produto {
 
+public class Produto {
+	private int codigo;
+	private String nome;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
