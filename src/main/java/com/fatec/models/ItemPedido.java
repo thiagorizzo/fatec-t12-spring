5faf48e0ae3ff1e7de5a677dc53d8c9f0fc4201f
Thiago Rizzo <thiagorizzo@gmail.com>
2019-10-26 10:39:05 -0200

    commit inicial

---
+++ src/main/java/com/fatec/models/ItemPedido.java
@@ -0,0 +1,22 @@
+package com.fatec.models;
+
+public class ItemPedido {
+	private int codigo;
+	private int quantidade;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	
+	public int getQuantidade() {
+		return quantidade;
+	}
+	
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+}
---
+++ src/main/java/com/fatec/models/Produto.java
@@ -0,0 +1,5 @@
+package com.fatec.models;
+
+public class Produto {
+
+}
